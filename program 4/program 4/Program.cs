04171b3109643151a98314c9e5afff8d2416fa11
ASHOKJAAT12 <ajat39963@gmail.com>
2026-07-11 00:22:33 +0530

    program 4

---
+++ program 4/program 4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+namespace name_4
+{
+    internal class program_4
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Enter A : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter b : ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The a + b is " + (a + b));
+        }
+    }
+}
