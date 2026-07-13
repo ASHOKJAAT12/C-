076247ac4fabd41f38bf24bc3d044f8d9fc59a57
ASHOKJAAT12 <ajat39963@gmail.com>
2026-07-13 22:59:09 +0530

    is prime program add

---
+++ prime number/prime number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace name
+{
+    class program
+    {
+        public static void Main(String[] args)
+        {
+            bool isPrime = true;
+            Console.WriteLine("enter the number : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for ( int i = 2; i <= n/2; i++ )
+            {
+                if ( n%i == 0 )
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (  isPrime )
+            {
+                Console.WriteLine("the number is prime.");
+            }
+            else
+            {
+                Console.WriteLine("The number is not prime.");
+            }
+        }
+    }
+
+}
