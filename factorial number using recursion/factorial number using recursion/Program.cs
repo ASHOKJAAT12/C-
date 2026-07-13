c6b6bcc11c94413a3380d091514fccfca3624907
ASHOKJAAT12 <ajat39963@gmail.com>
2026-07-13 23:34:45 +0530

    factorial number using recursion

---
+++ factorial number using recursion/factorial number using recursion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace name
+{
+    class program
+    {
+            public void calculate(int sum, int n)
+            {
+                sum *= n;
+                n--;
+            if (n == 0)
+            {
+                Console.WriteLine("the factorial is " + sum);
+            }
+            else
+            {
+                calculate(sum, n);
+            }
+        }
+        public static void Main(string[] args)
+        {
+            int sum = 1;
+            Console.WriteLine("Enter youor number : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            program a = new program();
+            a.calculate(sum, n);
+        }
+    }
+}
