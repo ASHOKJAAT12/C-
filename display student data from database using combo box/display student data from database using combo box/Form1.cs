d6107f651547a8349b008d01f65d7fcdb2fab783
ASHOKJAAT12 <ajat39963@gmail.com>
2026-07-14 23:39:16 +0530

    read data from database program

--- display student data from database using combo box/display student data from database using combo box/Form1.cs
+++ display student data from database using combo box/display student data from database using combo box/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
 namespace display_student_data_from_database_using_combo_box
 {
     public partial class Form1 : Form
@@ -15,6 +15,37 @@ namespace display_student_data_from_database_using_combo_box
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"data source=DESKTOP-GUSLB1E; initial catalog=juice; integrated security=true;");
+            con.Open();
+            string str = "select rollno from student1;";
+            SqlCommand cmd = new SqlCommand(str,con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            while(dr.Read())
+            {
+                comboBox1.Items.Add(dr.GetValue(0));
+            }
+            con.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"data source=DESKTOP-GUSLB1E; initial catalog=juice; integrated security=true;");
+            con.Open();
+            string str = "select * from student1 where rollno=" + comboBox1.Text + "";
+            SqlCommand cmd = new SqlCommand(str, con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            while ( dr.Read() )
+            {
+                tname.Text = dr.GetString(0);
+                tclass.Text = dr.GetString(1);
+                trollno.Text = dr.GetValue(2).ToString();
+                tsubject.Text = dr.GetString(3);
+            }
+            con.Close();
         }
     }
 }
